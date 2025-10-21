8da176d04b9c5f3b135fb7d78dd97fe072366a54
samy <2393410@cmontmorency.qc.ca>
2025-10-21 19:37:39 +0000

    [ADD] Constructeur par defaut de livre

---
+++ book.cpp
@@ -0,0 +1,18 @@
+#include <sstream>
+#include <algorithm>
+
+#include <book.h>
+
+using namespace std;
+
+
+//COnstructeur def
+Book::Book() : title("") , author("") , isbn("") {}
+
+// Constructeur avec param
+
+// Getter
+
+// Setters
+
+// Methods
