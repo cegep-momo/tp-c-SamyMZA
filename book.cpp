fa836f0c4ea3b0d04aca3b0cf0544472445e4bb1
samy <2393410@cmontmorency.qc.ca>
2025-10-24 23:31:06 +0000

    [ADD] Exportation de donne des livres

--- book.cpp
+++ book.cpp
@@ -5,49 +5,53 @@
 
 using namespace std;
 
-
-//COnstructeur def
-Book::Book() : title("") , author("") , isbn(""), isAvailable(true), borrowerName("") {}
+// COnstructeur def
+Book::Book() : title(""), author(""), isbn(""), isAvailable(true), borrowerName("") {}
 
 // Constructeur avec param
 
-Book::Book(const string& title,const string& author,const string&isbn) : title(title) , author(author) , isbn(isbn), isAvailable(true), borrowerName("") {}
+Book::Book(const string &title, const string &author, const string &isbn) : title(title), author(author), isbn(isbn), isAvailable(true), borrowerName("") {}
 
 // Getter
 
-string Book::getTitle() const {return title;}
-string Book::getAuthor() const {return author;}
-string Book::getISBN() const {return isbn;}
-bool Book::getAvailability() const {return isAvailable;}
-string Book ::getBorrowerName() const {return borrowerName;}
+string Book::getTitle() const { return title; }
+string Book::getAuthor() const { return author; }
+string Book::getISBN() const { return isbn; }
+bool Book::getAvailability() const { return isAvailable; }
+string Book ::getBorrowerName() const { return borrowerName; }
 // Setters
 
-void Book::setTitle(const string& title) {this->title = title;}
-void Book::setAuthor(const string& author) {this->author = author;}
-void Book::setISBN(const string& isbn) {this-> isbn = isbn;}
-void Book::setAvailability(bool available) {this->isAvailable = available;}
-void Book::setBorrowerName(const string& name) {this->borrowerName = name;}
+void Book::setTitle(const string &title) { this->title = title; }
+void Book::setAuthor(const string &author) { this->author = author; }
+void Book::setISBN(const string &isbn) { this->isbn = isbn; }
+void Book::setAvailability(bool available) { this->isAvailable = available; }
+void Book::setBorrowerName(const string &name) { this->borrowerName = name; }
 
 // Methods
 
-void Book::checkOut(const string& borrower){
+void Book::checkOut(const string &borrower)
+{
    setBorrowerName(borrower);
    setAvailability(false);
 }
 
-void Book::returnBook(){
+void Book::returnBook()
+{
    setBorrowerName("");
    setAvailability(true);
 }
 
-string Book::toString() const{
+string Book::toString() const
+{
    return "Titre: " + getTitle() + "\nAuteur: " + getAuthor() + "\nISBN: " + getISBN() + "\nStatus: " + (getAvailability() ? "Disponible" : "Emprunter par : " + getBorrowerName());
 }
 
-string Book::toFileFormat() const{
-
+string Book::toFileFormat() const
+{
+   return title + "|" + author + "|" + isbn + "|" + (isAvailable ? "1" : "0") + "|" + borrowerName;
 }
 
-void Book::fromFileFormat(const string& line){
+void Book::fromFileFormat(const string &line)
+{
 
 }
